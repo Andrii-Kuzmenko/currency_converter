78316f068096a3b3a03de530f0832122b109cab9
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-08-12 12:24:28 +0300

    add: SmallTitle component

---
+++ src/components/SmallTitle/SmallTitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './SmallTitle.module.scss';
+import classNames from 'classnames';
+
+type Props = {
+  children: string,
+  className?: string;
+}
+
+export const SmallTitle: React.FC<Props> = ({ children, className }) => (
+  <h1 className={classNames([styles.title], className)}>
+    {children}
+  </h1>
+);
