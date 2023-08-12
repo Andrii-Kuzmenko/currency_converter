93889d975de6d2eb2c7f2fade9ea20903e246a16
Andrii-Kuzmenko <andriikuzmenko35@gmail.com>
2023-08-12 17:43:52 +0300

    update: downloading data

--- src/api/dataCurrency.ts
+++ src/api/dataCurrency.ts
@@ -1,6 +1,6 @@
 import { DataCurrency } from '../types/DataCarrency';
 import { client } from '../utils/axiosClient';
 
-export const getCurrency = () => { 
-  return client.get<DataCurrency>('');
+export const getCurrency = (from: string, to: string) => {
+  return client.get<DataCurrency>('', from, to);
 };
--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,3 +1,23 @@
+// import axios from 'axios';
+
+// export const BASE_URL = 'https://api.frankfurter.app/latest';
+
+// const instance = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// export const client = {
+//   async get<T>(url: string, params: Record<string, unknown> = {from, to}) {
+//     params['amount'] = '1';
+//     // params['from'] = 'AUD';
+//     // params['to'] = 'CAD';
+
+//     const response = await instance.get<T>(url, { params });
+
+//     return response.data;
+//   },
+// };
+
 import axios from 'axios';
 
 export const BASE_URL = 'https://api.frankfurter.app/latest';
@@ -7,10 +27,12 @@ const instance = axios.create({
 });
 
 export const client = {
-  async get<T>(url: string, params: Record<string, unknown> = {}) {
-    params['amount'] = '1';
-    params['from'] = 'AUD';
-    params['to'] = 'CAD';
+  async get<T>(url: string, from: string, to: string) {
+    const params: Record<string, unknown> = {
+      amount: '1',
+      from: from,
+      to: to,
+    };
 
     const response = await instance.get<T>(url, { params });
 
